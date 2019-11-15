cb6e3987ad6b84845f6c0b3c1ed38ccd295d7d48
JKRuigu <ruigukjohn.com>
2019-11-15 15:34:34 +0300

    Removed insert.php and radio button

--- register_form.php
+++ register_form.php
@@ -1,3 +1,27 @@
+<?php 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	$fname = $_POST['fname'];
+	$lname = $_POST['lname'];
+	$age = $_POST['age'];
+	$course = $_POST['course'];
+	$adm_no = $_POST['adm_no'];
+	$department = $_POST['department'];
+	$school = $_POST['school'];
+	$gender = $_POST['gender'];
+	
+	if(!$fname || !$lname || !$age || !$course || !$adm_no || !$department || !$school || !$gender){
+		echo 'No data <a href="register_form.php">Register</a>';
+	}else{
+		$con = mysqli_connect("localhost","maureen","","mksu");
+
+		$sql = "INSERT INTO student(fname,lname,adm_no,age,school,department,course,gender) 
+					values('$fname','$lname','$adm_no','$age','$school ','$department','$course','$gender')";
+		
+		mysqli_query($con,$sql);
+	}
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -16,30 +40,30 @@
 <div class="col-md-6">
 <div class="card">
 <article class="card-body">
-<form action="insert.php" method="post">
+<form action = "<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
 	<div class="form-row">
 		<div class="col form-group">
 			<label>First name </label>   
-		  	<input type="text" name="fname" class="form-control" placeholder="">
+		  	<input type="text" name="fname" class="form-control" required>
 		</div>
 		<div class="col form-group">
 			<label>Last name</label>
-		  	<input type="text" name="lname" class="form-control" placeholder=" ">
+		  	<input type="text" name="lname" class="form-control" required>
 		</div>
 	</div>
 	<div class="form-row">
 	<div class="col form-group">
 		<label>Admission Number </label>   
-	  	<input type="text" class="form-control"  name="adm_no" placeholder="###-####-####">
+	  	<input type="text" class="form-control"  name="adm_no" placeholder="###-####-####" required>
 	</div>
 	<div class="col form-group">
 		<label>Age</label>
-	  	<input type="number" class="form-control" name="age" min="17" max="50">
+	  	<input type="number" class="form-control" name="age" min="17" max="50" required>
 	</div>
 	</div>
 	<div class="form-group">
 		<label>School</label>
-	    <input name="school" class="form-control" type="text">
+	    <input name="school" class="form-control" type="text" required>
 	</div>
 	<div class="form-row">
 		<div class="form-group col-md-6">
@@ -48,7 +72,7 @@
 		</div> 
 		<div class="form-group col-md-6">
 		  <label>Course</label>
-		  <select id="inputState" name="course" class="form-control">
+		  <select id="inputState" name="course" class="form-control" required>
 		    <option> Choose...</option>
 		      <option>Bsc IT</option>
 		      <option>Bsc Computer Science</option>
@@ -59,16 +83,16 @@
 		</div>
 	</div>	
 	
-	<div class="form-row">
-		<div class="form-group col-md-6">
-		  <label>Gender</label>
-		  <select id="inputState" name="gender" class="form-control">
-		    <option> Choose...</option>
-		      <option>Female</option>
-		      <option>Male</option>
-		  </select>
-		</div>
-	</div>	
+	<div class="form-group">
+		<label class="form-check form-check-inline">
+		  <input class="form-check-input" type="radio" name="gender" value="Male">
+		  <span class="form-check-label"> Male </span>
+		</label>
+		<label class="form-check form-check-inline">
+		  <input class="form-check-input" type="radio" name="gender" value="Female">
+		  <span class="form-check-label"> Female</span>
+		</label>
+	</div>
 
     <div class="form-group">
         <button type="submit" class="btn btn-primary btn-block"> Register  </button>
